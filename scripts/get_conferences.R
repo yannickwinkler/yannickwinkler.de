68d6bf12e96d304a89f0d160152fd4cfcdbe0172
Yannick Winkler <yannickwinkler@gmx.net>
2026-01-15 14:19:36 +0100

    update conferences

--- scripts/get_conferences.R
+++ scripts/get_conferences.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 
 generate_conference_list <- function(json_path, highlight_author = "Winkler, Y.", entry_type = "conference") {
-  # Daten einlesen und filtern
   conferences <- jsonlite::read_json(json_path, simplifyVector = FALSE) %>%
     keep(~ .x$type == entry_type)
   
@@ -9,47 +8,56 @@ generate_conference_list <- function(json_path, highlight_author = "Winkler, Y."
     return(str_c("No entries of type '", entry_type, "' found."))
   }
   
-  # Nach Jahr sortieren (absteigend)
-  conferences <- conferences %>%
-    enframe(name = NULL) %>%
-    mutate(year = map_int(value, "year", .default = NA_integer_)) %>%
+  # Convert to tibble and format entries
+  conf_tbl <- tibble(
+    year = map_int(conferences, "year", .default = NA_integer_),
+    conf = conferences
+  ) %>%
     arrange(desc(year), na.last = TRUE) %>%
-    pull(value)
-  
-  # Markdown generieren
-  output_markdown <- conferences %>%
-    map_chr(function(conf) {
-      # Auto-bold highlighted author
-      escaped_author <- str_replace_all(highlight_author, "([\\W])", "\\\\\\1")
-      formatted_authors <- conf$authors %>%
-        str_replace_all(
-          regex(escaped_author, ignore_case = TRUE),
-          ~ str_c("**", .x, "**")
-        )
-
-      # Titel-Formatierung
-      title_suffix <- if(str_detect(conf$title, "[.!?]$")) " " else ". "
-      
-      # Eintrag zusammenbauen
-      entry_details <- str_c(
-        formatted_authors, " (", conf$year, "). ",
-        "<i>", conf$title, "</i>", title_suffix,
-        conf$conference, ", ",
-        conf$location
-      )
-      
-      # Slides hinzufügen, wenn verfügbar
-      if(!is.null(conf$slides_url) && str_length(conf$slides_url) > 0) {
+    mutate(
+      formatted_entry = map_chr(conf, function(conf) {
+        # Auto-bold highlighted author
+        escaped_author <- str_replace_all(highlight_author, "([\\W])", "\\\\\\1")
+        formatted_authors <- conf$authors %>%
+          str_replace_all(
+            regex(escaped_author, ignore_case = TRUE),
+            ~ str_c("**", .x, "**")
+          )
+        
+        title_suffix <- if(str_detect(conf$title, "[.!?]$")) " " else ". "
         entry_details <- str_c(
-          entry_details, " [<i class='fas fa-project-diagram'></i> Slides](", 
-          conf$slides_url, ")"
+          formatted_authors, " (", conf$year, "). ",
+          "<i>", conf$title, "</i>", title_suffix,
+          conf$conference, ", ",
+          conf$location
         )
-      }
-      
-      # Markdown-String erstellen
-      str_c("\n\n**", conf$year, "**\n\n- ", entry_details, "\n")
-    })
+        
+        if(!is.null(conf$slides_url) && str_length(conf$slides_url) > 0) {
+          entry_details <- str_c(
+            entry_details, " [<i class='fas fa-project-diagram'></i> Slides](",
+            conf$slides_url, ")"
+          )
+        }
+        
+        str_c("- ", entry_details)
+      })
+    )
   
-  # Alle Einträge zusammenfügen
-  str_c(output_markdown, collapse = "")
+  # Group by year and create markdown sections
+  output_markdown <- conf_tbl %>%
+    arrange(desc(year)) %>%
+    group_by(year) %>%
+    summarise(
+      entries = str_c(formatted_entry, collapse = "\n"),
+      .groups = "drop"
+    ) %>%
+    # ADD THIS LINE to re-sort the summarized data by year in descending order
+    arrange(desc(year)) %>% 
+    mutate(
+      year_section = str_c("\n\n**", year, "**\n\n", entries, "\n")
+    ) %>%
+    pull(year_section) %>%
+    str_c(collapse = "")
+  
+  return(output_markdown)
 }
